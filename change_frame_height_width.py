b4d5e913585ed8fca435e66ce979a33c9758cea6
sunzid <smahmud@houseofkaizen.com>
2021-01-27 20:26:18 +0600

    Setting Camera Parameters

---
+++ change_frame_height_width.py
@@ -0,0 +1,25 @@
+import cv2
+
+# select video from default camera
+cap = cv2.VideoCapture(0)
+
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1366)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
+
+# show video on frame
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
